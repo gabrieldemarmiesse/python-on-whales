c3954087b071094b1f1a71ef6a2a7da716050494
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-09-02 23:41:48 +0200

    Add command line to download cli and tests (#2)

--- tests/docker_cli_wrapper/components/test_download_binaries.py
+++ tests/docker_cli_wrapper/components/test_download_binaries.py
@@ -1,7 +1,40 @@
-from docker_cli_wrapper.components.download_binaries import download_from_url
+from docker_cli_wrapper.components import download_binaries
+from docker_cli_wrapper.utils import run
 
 
 def test_download_from_url(tmp_path):
     url = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
-    download_from_url(url, tmp_path / "dodo.png")
+    download_binaries.download_from_url(url, tmp_path / "dodo.png")
     assert (tmp_path / "dodo.png").exists()
+
+
+def test_download_cli():
+    try:
+        download_binaries.DOCKER_BINARY_PATH.unlink()
+    except FileNotFoundError:
+        pass
+    download_binaries.download_docker_cli()
+    simple_command = [
+        download_binaries.DOCKER_BINARY_PATH,
+        "run",
+        "--rm",
+        "hello-world",
+    ]
+    output = run(simple_command, capture_stdout=True, capture_stderr=True)
+    assert "Hello from Docker!" in output
+
+
+def test_download_cli_from_cli():
+    try:
+        download_binaries.DOCKER_BINARY_PATH.unlink()
+    except FileNotFoundError:
+        pass
+    run(["docker-cli-wrapper", "download-cli"])
+    simple_command = [
+        download_binaries.DOCKER_BINARY_PATH,
+        "run",
+        "--rm",
+        "hello-world",
+    ]
+    output = run(simple_command, capture_stdout=True, capture_stderr=True)
+    assert "Hello from Docker!" in output
