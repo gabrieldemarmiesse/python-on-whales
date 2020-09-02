60257f2875d83d06dca24104a83b0aa1c3b6e5c4
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-09-02 07:53:43 +0000

    Added requests and tqdm.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="docker-cli-wrapper",
     version=0.1,
-    install_requires=["typeguard", "pydantic"],
+    install_requires=["typeguard", "pydantic", "requests", "tqdm"],
     packages=find_packages(),
 )
