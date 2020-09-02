85e1a87e7353abae812a30618fbaf469ac607317
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-09-02 21:37:32 +0000

    Changed the declaration of dependencies.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 setup(
     name="docker-cli-wrapper",
     version=0.1,
-    install_requires=["typeguard", "pydantic", "requests", "tqdm"],
+    install_requires=Path("requirements.txt").read_text().splitlines(),
     packages=find_packages(),
     entry_points={
         "console_scripts": [
