26a9fd0473fb1c187bc36d1a0303bccc6436445b
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-09-04 19:40:48 +0200

    Auto download of cli if needed (#8)

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,5 +1,9 @@
 set -e -x
 
 
-DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build -f tests/Dockerfile -t test-image-python-on-whales .
+DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build -f tests/Dockerfile --target tests_with_cli -t test-image-python-on-whales .
+docker run -v /var/run/docker.sock:/var/run/docker.sock test-image-python-on-whales
+
+
+DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build -f tests/Dockerfile --target tests_without_cli -t test-image-python-on-whales .
 docker run -v /var/run/docker.sock:/var/run/docker.sock test-image-python-on-whales
