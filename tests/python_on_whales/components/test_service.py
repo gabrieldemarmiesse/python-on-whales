25982dfbf8b773265c347b8f8440672c8f4d01bc
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-11-15 15:12:57 +0000

    Added the code to create and leave the swarm.

--- tests/python_on_whales/components/test_service.py
+++ tests/python_on_whales/components/test_service.py
@@ -7,12 +7,14 @@ from python_on_whales.utils import PROJECT_ROOT
 
 @pytest.fixture
 def with_test_stack():
+    docker.swarm.init()
     docker.stack.deploy(
         "some_stack",
         [PROJECT_ROOT / "tests/python_on_whales/components/test-stack-file.yml"],
     )
     yield
     docker.stack.remove("some_stack")
+    docker.swarm.leave(force=True)
 
 
 @pytest.mark.usefixtures("with_test_stack")
