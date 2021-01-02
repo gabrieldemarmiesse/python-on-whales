0bd5be896b7d086d5e11f6cd3951d2de72efcf6e
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2021-01-02 02:50:06 +0000

    Added the Context object as well as the remove function.

---
+++ tests/python_on_whales/components/test_context.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import pytest
+
+from python_on_whales.components.context import ContextInspectResult
+
+
+def get_all_jsons():
+    jsons_directory = Path(__file__).parent / "contexts"
+    return sorted(list(jsons_directory.iterdir()))
+
+
+@pytest.mark.parametrize("json_file", get_all_jsons())
+def test_load_json(json_file):
+    json_as_txt = json_file.read_text()
+    ContextInspectResult.parse_raw(json_as_txt)
+    # we could do more checks here if needed
