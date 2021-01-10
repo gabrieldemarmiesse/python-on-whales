1bbb34d6b69f88f209ce953341a37d0639d146b3
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2021-01-10 16:46:55 +0000

    Added swarm ca.

--- README.md
+++ README.md
@@ -174,7 +174,7 @@ Use the right tool for the right job 🙂
 | secret  | 4/4 | ![50%](https://progress-bar.dev/100) |
 | service  | 7/9 | ![50%](https://progress-bar.dev/78) |
 | stack  | 4/5 | ![50%](https://progress-bar.dev/80) |
-| swarm  | 5/8 | ![50%](https://progress-bar.dev/62) |
+| swarm  | 6/8 | ![50%](https://progress-bar.dev/75) |
 | system  | 3/4 | ![50%](https://progress-bar.dev/75) |
 | trust  | 0/3 | ![50%](https://progress-bar.dev/0) |
 | volume  | 7/7 | ![50%](https://progress-bar.dev/100) |
--- tests/python_on_whales/components/test_swarm.py
+++ tests/python_on_whales/components/test_swarm.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import pytest
 
 from python_on_whales import docker
@@ -10,6 +12,16 @@ def test_swarm_update_retention_limit():
         docker.system.info().swarm.cluster.spec.orchestration.task_history_retention_limit
         == 4
     )
+
+
+@pytest.mark.usefixtures("swarm_mode")
+def test_swarm_change_certificate_expiry():
+    docker.swarm.ca(certificate_expiry=timedelta(days=1), rotate=True)
+    info = docker.system.info()
+    node_cert_expiry = timedelta(
+        microseconds=info.swarm.cluster.spec.ca_config.node_cert_expiry / 1000
+    )
+    assert node_cert_expiry == timedelta(days=1)
 
 
 @pytest.mark.usefixtures("swarm_mode")
