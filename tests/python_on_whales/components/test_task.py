59da28641969bcfae9ad4905dadf387f6c6485dc
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2021-01-13 19:25:19 +0000

    Added docker.task.list()

--- tests/python_on_whales/components/test_task.py
+++ tests/python_on_whales/components/test_task.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pytest
 
+from python_on_whales import docker
 from python_on_whales.components.task import TaskInspectResult
 
 
@@ -15,3 +16,15 @@ def test_load_json(json_file):
     json_as_txt = json_file.read_text()
     TaskInspectResult.parse_raw(json_as_txt)
     # we could do more checks here if needed
+
+
+@pytest.mark.usefixtures("swarm_mode")
+def test_list_tasks():
+    service = docker.service.create("busybox", ["sleep", "infinity"])
+
+    # Todo: use a context manager
+    tasks = docker.task.list()
+    assert len(tasks) >= 1
+    assert tasks[0].desired_state == "running"
+    assert tasks[0].service_id == service.id
+    docker.service.remove(service)
