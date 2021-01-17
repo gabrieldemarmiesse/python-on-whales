9017c4af5c479c7588a9b3d45e0d7472cd8b16cb
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2021-01-17 13:12:34 +0000

    Added some docs for plugins.

--- docs/autogen.py
+++ docs/autogen.py
@@ -43,6 +43,9 @@ pages = {
         "python_on_whales.components.network.NetworkCLI"
     ),
     "sub-commands/node.md": get_methods("python_on_whales.components.node.NodeCLI"),
+    "sub-commands/plugin.md": get_methods(
+        "python_on_whales.components.plugin.PluginCLI"
+    ),
     "sub-commands/secret.md": get_methods(
         "python_on_whales.components.secret.SecretCLI"
     ),
@@ -65,6 +68,7 @@ pages = {
     "docker_objects/images.md": get_methods("python_on_whales.Image"),
     "docker_objects/networks.md": get_methods("python_on_whales.Network"),
     "docker_objects/nodes.md": get_methods("python_on_whales.Node"),
+    "docker_objects/plugins.md": get_methods("python_on_whales.Plugin"),
     "docker_objects/services.md": get_methods("python_on_whales.Service"),
     "docker_objects/stacks.md": get_methods("python_on_whales.Stack"),
     "docker_objects/volumes.md": get_methods("python_on_whales.Volume"),
@@ -84,6 +88,7 @@ class MyDocumentationGenerator(DocumentationGenerator):
         signature = signature.replace("ManifestCLI.", "docker.manifest.")
         signature = signature.replace("NetworkCLI.", "docker.network.")
         signature = signature.replace("NodeCLI.", "docker.node.")
+        signature = signature.replace("PluginCLI.", "docker.plugin.")
         signature = signature.replace("SecretCLI.", "docker.secret.")
         signature = signature.replace("ServiceCLI.", "docker.service.")
         signature = signature.replace("StackCLI.", "docker.stack.")
@@ -105,6 +110,7 @@ doc_generator = MyDocumentationGenerator(
         "python_on_whales.Image",
         "python_on_whales.Network",
         "python_on_whales.Node",
+        "python_on_whales.Plugin",
         "python_on_whales.Service",
         "python_on_whales.Stack",
         "python_on_whales.Task",
