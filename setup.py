4dcc15cc0c0244814956f3729e4d731cc90ef4ea
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2021-03-05 10:46:44 +0100

    Bump version to 0.14.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_long_description() -> str:
 
 setup(
     name="python-on-whales",
-    version="0.14.2",
+    version="0.14.3",
     description="A Docker client for Python, designed to be fun and intuivive!",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
