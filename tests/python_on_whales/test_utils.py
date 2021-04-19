28feb5601bcefd5b115c408f94b8692356a72d5b
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2021-04-19 11:38:10 +0200

    Fix environment variables propagation. (#159)

--- tests/python_on_whales/test_utils.py
+++ tests/python_on_whales/test_utils.py
@@ -3,3 +3,14 @@ import python_on_whales.utils
 
 def test_project_root():
     assert (python_on_whales.utils.PROJECT_ROOT / "setup.py").exists()
+
+
+def test_environment_variables_propagation(monkeypatch):
+    monkeypatch.setenv("SOME_VARIABLE", "dododada")
+
+    stdout = python_on_whales.utils.run(
+        ["bash", "-c", "echo $SOME_VARIABLE && echo $OTHER_VARIABLE"],
+        capture_stdout=True,
+        env={"OTHER_VARIABLE": "dudu"},
+    )
+    assert stdout == "dododada\ndudu"
