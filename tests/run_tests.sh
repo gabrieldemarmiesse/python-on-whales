96ee741b83dc22f295b7263ebe6d22a904049cf2
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2023-02-09 14:40:46 +0100

    Update tests/run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,7 +2,7 @@ set -e -x
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-docker buildx bake
+docker buildx bake --load
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock tests-with-binaries-python-on-whales
 docker run -v /var/run/docker.sock:/var/run/docker.sock tests-without-any-binary-python-on-whales
