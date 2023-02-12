e15f3706d5be432854707e99506f5bf899f7589f
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2023-02-12 11:58:19 +0000

    Quiet

--- tests/ci-setup.sh
+++ tests/ci-setup.sh
@@ -9,7 +9,7 @@ pip install -e .
 pip install -r ./tests/test-requirements.txt
 
 mkdir -p ~/.docker/cli-plugins/
-wget https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64 -O ~/.docker/cli-plugins/docker-compose
+wget -q https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64 -O ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
-wget https://github.com/docker/buildx/releases/download/v0.10.0/buildx-v0.10.0.linux-amd64 -O ~/.docker/cli-plugins/docker-buildx
+wget -q https://github.com/docker/buildx/releases/download/v0.10.0/buildx-v0.10.0.linux-amd64 -O ~/.docker/cli-plugins/docker-buildx
 chmod +x ~/.docker/cli-plugins/docker-buildx
