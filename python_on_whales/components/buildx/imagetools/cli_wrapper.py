3641099e28ba2e3b6e4908a3218d327745ad89a2
Darren P <36205316+darpham@users.noreply.github.com>
2023-03-30 06:49:58 -0700

    Add Imagetools create command (#419)

--- python_on_whales/components/buildx/imagetools/cli_wrapper.py
+++ python_on_whales/components/buildx/imagetools/cli_wrapper.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import List, Optional, Union
+
 from python_on_whales.client_config import DockerCLICaller
 from python_on_whales.utils import run
 
@@ -10,3 +13,55 @@ class ImagetoolsCLI(DockerCLICaller):
         full_cmd = self.docker_cmd + ["buildx", "imagetools", "inspect", "--raw", name]
         result = run(full_cmd)
         return Manifest.parse_raw(result)
+
+    def create(
+        self,
+        sources: List[str] = [],
+        tags: List[str] = [],
+        append: bool = False,
+        files: List[Union[str, Path]] = [],
+        dry_run: bool = False,
+        builder: Optional[str] = None,
+    ) -> Optional[Manifest]:
+        """
+        Create a new manifest list based on source manifests.
+        The source manifests can be manifest lists or single platform distribution manifests and
+        must already exist in the registry where the new manifest is created.
+        If only one source is specified, create performs a carbon copy.
+
+        The CLI docs is [here](https://docs.docker.com/engine/reference/commandline/buildx_imagetools_create/)
+        and it contains a lot more information.
+
+        # Arguments
+            sources: The sources manifest to create, change
+            append: Append to existing manifest
+            dry_run: Show final image instead of pushing
+            files: Read source descriptor from file
+            builder: The builder to use.
+        """
+        if not isinstance(sources, list):
+            raise TypeError(
+                "The argument 'sources' of the function docker.buildx.imagetools.create() must be a list of strings."
+            )
+        if not isinstance(tags, list):
+            raise TypeError(
+                "The argument 'tags' of the function docker.buildx.imagetools.create() must be a list of strings."
+            )
+        if not isinstance(files, list):
+            raise TypeError(
+                "The argument 'files' of the function docker.buildx.imagetools.create() must be a list of strings."
+            )
+
+        full_cmd = self.docker_cmd + ["buildx", "imagetools", "create"]
+        for tag in tags:
+            full_cmd.add_simple_arg("--tag", tag)
+        for file in files:
+            full_cmd.add_simple_arg("--file", file)
+        full_cmd.add_simple_arg("--builder", builder)
+        full_cmd.add_flag("--append", append)
+        full_cmd.add_flag("--dry-run", dry_run)
+        full_cmd += sources
+
+        result = run(full_cmd)
+        if dry_run:
+            return Manifest.parse_raw(result)
--- tests/python_on_whales/components/buildx/imagetools/test_imagetools_cli_wrapper.py
+++ tests/python_on_whales/components/buildx/imagetools/test_imagetools_cli_wrapper.py
@@ -1,4 +1,8 @@
 from python_on_whales import docker
+from python_on_whales.utils import PROJECT_ROOT
+
+bake_test_dir = PROJECT_ROOT / "tests/python_on_whales/components/bake_tests"
+bake_file = bake_test_dir / "docker-bake.hcl"
 
 
 def test_imagetools_inspect_multiarch():
@@ -12,3 +16,55 @@ def test_imagetools_inspect_single_image():
     )
     assert a.schema_version == 2
     assert a.config.media_type.startswith("application/")
+
+
+def test_imagetools_create_single_image():
+    a = docker.buildx.imagetools.create(["python:3.7.0"], dry_run=True)
+    assert a.media_type.startswith("application/")
+
+
+def test_imagetools_create_single_image_with_hash():
+    a = docker.buildx.imagetools.create(
+        [
+            "python@sha256:b48b88687b1376f3135a048c8cdaccad4e8dd1af2f345c693a39b75a5683a3eb"
+        ],
+        dry_run=True,
+    )
+    assert a.schema_version == 2
+    assert a.media_type.startswith("application/")
+
+
+def test_imagetools_create_new_image_with_tag(docker_registry):
+    busybox = docker.pull("busybox:1")
+    base_image = f"{docker_registry}/test:image1"
+    docker.tag(busybox, base_image)
+    docker.push(base_image)
+    new_image = f"{docker_registry}/test:image2"
+    docker.buildx.imagetools.create([base_image], tags=[new_image])
+    docker.pull(new_image)
+
+
+def test_imagetools_create_new_image_with_tags(docker_registry):
+    busybox = docker.pull("busybox:1")
+    base_image = f"{docker_registry}/test:image1"
+    docker.tag(busybox, base_image)
+    docker.push(base_image)
+    new_image_2 = f"{docker_registry}/test:image2"
+    new_image_3 = f"{docker_registry}/test:image3"
+    docker.buildx.imagetools.create([base_image], tags=[new_image_2, new_image_3])
+    docker.pull([new_image_2, new_image_3])
+
+
+def test_imagetools_append(docker_registry):
+    busybox = docker.pull("busybox:1")
+    alpine = docker.pull("alpine")
+    base_image_busybox = f"{docker_registry}/test:busybox"
+    base_image_alpine = f"{docker_registry}/test:alpine"
+    docker.tag(busybox, base_image_busybox)
+    docker.tag(alpine, base_image_alpine)
+    docker.push(base_image_busybox)
+    docker.push(base_image_alpine)
+    new_image = f"{docker_registry}/test:image2"
+    docker.buildx.imagetools.create([base_image_busybox], tags=[new_image])
+    docker.buildx.imagetools.create([base_image_alpine], tags=[new_image], append=True)
+    docker.pull(new_image)
