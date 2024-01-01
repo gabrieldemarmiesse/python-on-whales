4d1839b2c1fb2b1cc25315eb53f0761fd04caa7b
Lewis Gaul <lewis.gaul@gmail.com>
2024-01-01 17:46:38 +0000

    Put pip setup back in ci-setup.sh

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -7,3 +7,7 @@ THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
 "$THIS_DIR"/add-local-docker-registry.sh
 "$THIS_DIR"/download-docker-plugins.sh
 docker info
+
+pip install -U pip wheel
+pip install -e ./
+pip install -r tests/test-requirements.txt
