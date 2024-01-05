cb0235c5b62dc87a9c957e2c457b725c7611e5a1
Lewis Gaul <lewis.gaul@gmail.com>
2024-01-05 17:03:02 +0000

    Tidy up test/SA CI run (#517)

---
+++ scripts/add-local-docker-registry.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+cat /etc/docker/daemon.json
+echo '{"insecure-registries" : [ "localhost:5000" ]}' | sudo tee /etc/docker/daemon.json
+sudo service docker restart
+sleep 2
---
+++ scripts/ci-setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+"$THIS_DIR"/add-local-docker-registry.sh
+"$THIS_DIR"/download-docker-plugins.sh
+docker info
+
+pip install -U pip wheel
+pip install -e ./
+pip install -r tests/test-requirements.txt
