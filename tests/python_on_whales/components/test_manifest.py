b3fb1f15e88824e2668a4e433f2dd7f4d925d904
Lewis Gaul <lewis.gaul@gmail.com>
2024-01-15 16:28:17 +0000

    Move most of the remaining tests to use docker_client or ctr_client fixture

--- tests/python_on_whales/components/test_manifest.py
+++ tests/python_on_whales/components/test_manifest.py
@@ -1,9 +1,10 @@
 import json
 import os
+from typing import Generator
 
 import pytest
 
-from python_on_whales import docker
+from python_on_whales import DockerClient, Image
 from python_on_whales.components.manifest.cli_wrapper import (
     ManifestList,
     ManifestListInspectResult,
@@ -12,26 +13,30 @@ from python_on_whales.test_utils import get_all_jsons, random_name
 
 
 @pytest.fixture
-def with_manifest():
+def manifest(docker_client: DockerClient) -> Generator[ManifestList, None, None]:
     manifest_name = random_name()
     # utilizing old, pre-manifest-list images
     images = ["busybox:1.26", "busybox:1.27.1"]
-    docker.image.pull(images)
-    with docker.manifest.create(manifest_name, images) as my_manifest:
+    docker_client.image.pull(images)
+    with docker_client.manifest.create(manifest_name, images) as my_manifest:
         yield my_manifest
-    docker.image.remove(images)
+    docker_client.image.remove(images)
 
 
 @pytest.fixture
-def with_platform_variant_manifest(request):
+def platform_variant_manifest(
+    docker_client: DockerClient, request: pytest.FixtureRequest
+) -> Image:
     image_with_platform_variant = "arm32v7/busybox:1.35"
 
     def remove_docker_image():
-        docker.image.remove(image_with_platform_variant)
+        docker_client.image.remove(image_with_platform_variant)
 
     request.addfinalizer(remove_docker_image)
-    docker.image.pull(image_with_platform_variant, quiet=True, platform="linux/arm/v7")
-    return docker.image.inspect(image_with_platform_variant)
+    docker_client.image.pull(
+        image_with_platform_variant, quiet=True, platform="linux/arm/v7"
+    )
+    return docker_client.image.inspect(image_with_platform_variant)
 
 
 @pytest.mark.parametrize("json_file", get_all_jsons("manifests"))
@@ -45,23 +50,23 @@ def test_load_json(json_file):
     os.environ.get("CI", "false") == "true",
     reason="The creation of manifest doesn't work in github actions for some reason",
 )
-def test_manifest_create_remove(with_manifest):
-    assert isinstance(with_manifest, ManifestList)
+def test_manifest_create_remove(docker_client: DockerClient, manifest: ManifestList):
+    assert isinstance(manifest, ManifestList)
 
 
 @pytest.mark.skipif(
     os.environ.get("CI", "false") == "true",
     reason="The creation of manifest doesn't work in github actions for some reason",
 )
-def test_manifest_annotate(with_manifest):
-    docker.manifest.annotate(
-        with_manifest.name, "busybox:1.26", os="linux", arch="arm64"
+def test_manifest_annotate(docker_client: DockerClient, manifest: ManifestList):
+    docker_client.manifest.annotate(
+        manifest.name, "busybox:1.26", os="linux", arch="arm64"
     )
-    assert with_manifest.manifests[0].platform.os == "linux"
-    assert with_manifest.manifests[0].platform.architecture == "arm64"
+    assert manifest.manifests[0].platform.os == "linux"
+    assert manifest.manifests[0].platform.architecture == "arm64"
 
 
-def test_manifest_platform_variant(with_platform_variant_manifest):
-    assert "linux" in repr(with_platform_variant_manifest.os)
-    assert "arm" in repr(with_platform_variant_manifest.architecture)
-    assert "v7" in repr(with_platform_variant_manifest.variant)
+def test_manifest_platform_variant(platform_variant_manifest: Image):
+    assert "linux" in repr(platform_variant_manifest.os)
+    assert "arm" in repr(platform_variant_manifest.architecture)
+    assert "v7" in repr(platform_variant_manifest.variant)
--- tests/python_on_whales/components/test_plugin.py
+++ tests/python_on_whales/components/test_plugin.py
@@ -4,7 +4,7 @@ from typing import List
 
 import pytest
 
-from python_on_whales import docker
+from python_on_whales import DockerClient
 from python_on_whales.components.plugin.models import PluginInspectResult
 from python_on_whales.test_utils import get_all_jsons
 
@@ -23,23 +23,23 @@ def test_load_json(json_file):
     # we could do more checks here if needed
 
 
-def test_install_plugin_disable_enable():
-    with docker.plugin.install(test_plugin_name) as my_plugin:
+def test_install_plugin_disable_enable(docker_client: DockerClient):
+    with docker_client.plugin.install(test_plugin_name) as my_plugin:
         my_plugin.disable()
         my_plugin.enable()
-        assert my_plugin in docker.plugin.list()
+        assert my_plugin in docker_client.plugin.list()
         assert "name='vieux/sshfs:latest'" in repr(my_plugin)
 
 
-def test_plugin_upgrade():
-    with docker.plugin.install(test_plugin_name) as my_plugin:
+def test_plugin_upgrade(docker_client: DockerClient):
+    with docker_client.plugin.install(test_plugin_name) as my_plugin:
         my_plugin.disable()
         my_plugin.upgrade()
 
 
-def test_remove_empty_plugin_list():
-    with docker.plugin.install(test_plugin_name) as my_plugin:
-        plugins_set = set(docker.plugin.list())
+def test_remove_empty_plugin_list(docker_client: DockerClient):
+    with docker_client.plugin.install(test_plugin_name) as my_plugin:
+        plugins_set = set(docker_client.plugin.list())
         assert my_plugin in plugins_set
-        docker.plugin.remove([])
-        assert set(docker.plugin.list()) == plugins_set
+        docker_client.plugin.remove([])
+        assert set(docker_client.plugin.list()) == plugins_set
