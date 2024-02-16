6e5a7a919518cb1c91b965007d6767e7d219d06e
Lewis Gaul <lewis.gaul@gmail.com>
2024-02-16 18:51:07 +0000

    Add support for podman `--env-host` flag when creating containers (#551)

--- python_on_whales/components/container/cli_wrapper.py
+++ python_on_whales/components/container/cli_wrapper.py
@@ -560,6 +560,7 @@ class ContainerCLI(DockerCLICaller):
         entrypoint: Optional[str] = None,
         envs: Dict[str, str] = {},
         env_files: Union[ValidPath, List[ValidPath]] = [],
+        env_host: bool = False,
         expose: Union[int, List[int]] = [],
         gpus: Union[int, str, None] = None,
         groups_add: List[str] = [],
@@ -697,6 +698,7 @@ class ContainerCLI(DockerCLICaller):
 
         full_cmd.add_args_list("--env", format_dict_for_cli(envs))
         full_cmd.add_args_list("--env-file", env_files)
+        full_cmd.add_flag("--env-host", env_host)
 
         full_cmd.add_args_list("--expose", expose)
 
@@ -1276,6 +1278,7 @@ class ContainerCLI(DockerCLICaller):
         entrypoint: Optional[str] = None,
         envs: Dict[str, str] = {},
         env_files: Union[ValidPath, List[ValidPath]] = [],
+        env_host: bool = False,
         expose: Union[int, List[int]] = [],
         gpus: Union[int, str, None] = None,
         groups_add: List[str] = [],
@@ -1437,6 +1440,8 @@ class ContainerCLI(DockerCLICaller):
             envs: Environment variables as a `dict`.
                 For example: `{"OMP_NUM_THREADS": 3}`
             env_files: One or a list of env files.
+            env_host: Use host environment inside the container. Only supported
+                with podman.
             gpus: For this to work, you need the
                 [Nvidia container runtime](https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#install-guide)
                 The value needed is a `str` or `int`. Some examples of valid argument
@@ -1573,6 +1578,7 @@ class ContainerCLI(DockerCLICaller):
 
         full_cmd.add_args_list("--env", format_dict_for_cli(envs))
         full_cmd.add_args_list("--env-file", env_files)
+        full_cmd.add_flag("--env-host", env_host)
 
         full_cmd.add_args_list("--expose", expose)
 
