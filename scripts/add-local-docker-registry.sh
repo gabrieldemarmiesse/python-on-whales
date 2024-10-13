76e85551d6e9c0825cb995be354d7e70e5e4d27b
Lewis Gaul <lewis.gaul@gmail.com>
2024-10-13 20:01:44 +0100

    Fix the CI setup and podman testcase (#638)
    - Podman is now available in the default Ubuntu apt repos
    - Docker now doesn't come with the daemon config file
    - Newer version of podman has support for something that was previously an xfail testcase

--- scripts/add-local-docker-registry.sh
+++ scripts/add-local-docker-registry.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cat /etc/docker/daemon.json
+cat /etc/docker/daemon.json || true
 echo '{"insecure-registries" : [ "localhost:5000" ]}' | sudo tee /etc/docker/daemon.json
 sudo service docker restart
 sleep 2
