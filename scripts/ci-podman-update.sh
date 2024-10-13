3f8f1a38f6d435d19aeb2a633aa7dcda1504fe66
Lewis Gaul <lewis.gaul@gmail.com>
2024-10-13 19:03:22 +0100

    Fix CI

--- scripts/ci-podman-update.sh
+++ scripts/ci-podman-update.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-ubuntu_version='22.04'
-key_url="https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_${ubuntu_version}/Release.key"
-sources_url="https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_${ubuntu_version}"
-
-echo "deb $sources_url/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list
-curl -fsSL $key_url | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/devel_kubic_libcontainers_unstable.gpg > /dev/null
 sudo apt update -y
 sudo apt install -y podman
