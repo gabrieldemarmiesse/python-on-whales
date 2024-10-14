74a712352f01b54b09b2df69f27741ae5d747625
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2024-10-14 10:54:57 +0000

    Display an error message if using the `python-on-whales` command

---
+++ python_on_whales/command_line_entrypoint.py
@@ -0,0 +1,8 @@
+def main():
+    print(
+        "The python-on-whales command line interface has been removed "
+        "in python-on-whales v0.74.0. If you were using it to download the docker CLI, "
+        "you should instead do it manually. "
+        "Take a look at https://gabrieldemarmiesse.github.io/python-on-whales/docker_client/ "
+        "which has instructions to download the Docker client."
+    )
