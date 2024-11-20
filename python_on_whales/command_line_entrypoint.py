a2f95b07dac7084f4bf1e30207fcbf2823e36df9
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2024-11-20 12:50:44 +0100

    Add anchor to link being displayed when using CLI (#654)

--- python_on_whales/command_line_entrypoint.py
+++ python_on_whales/command_line_entrypoint.py
@@ -3,6 +3,6 @@ def main():
         "The python-on-whales command line interface has been removed "
         "in python-on-whales v0.74.0. If you were using it to download the docker CLI, "
         "you should instead do it manually. "
-        "Take a look at https://gabrieldemarmiesse.github.io/python-on-whales/docker_client/ "
+        "Take a look at https://gabrieldemarmiesse.github.io/python-on-whales/docker_client/#the-dockerpodman-cli "
         "which has instructions to download the Docker client."
     )
