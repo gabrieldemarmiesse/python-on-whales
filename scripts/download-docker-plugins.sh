caf6de95ce7428a923b5599168cd961b59154862
R3gardless <pidaoh@g.skku.edu>
2025-08-28 08:47:12 +0900

    feat: ✨ Update buildx version in download script
    - Changed buildx version from 0.27.0 to 0.13.0 for compatibility.
    - Ensured the latest version is downloaded for improved functionality.

--- scripts/download-docker-plugins.sh
+++ scripts/download-docker-plugins.sh
@@ -5,5 +5,5 @@ set -e
 mkdir -p ~/.docker/cli-plugins/
 wget -q https://github.com/docker/compose/releases/download/v2.39.0/docker-compose-linux-x86_64 -O ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
-wget -q https://github.com/docker/buildx/releases/download/v0.27.0/buildx-v0.27.0.linux-amd64 -O ~/.docker/cli-plugins/docker-buildx
+wget -q https://github.com/docker/buildx/releases/download/v0.13.0/buildx-v0.13.0.linux-amd64 -O ~/.docker/cli-plugins/docker-buildx
 chmod +x ~/.docker/cli-plugins/docker-buildx
