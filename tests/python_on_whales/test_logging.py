0ed1ed457a42fdd67cc2020db7fa8af8b2b3611a
Hasnain Zaidi <hasnainaz@googlemail.com>
2025-11-25 03:55:28 -0800

    Updated tests to reflect change in logging setup

--- tests/python_on_whales/test_logging.py
+++ tests/python_on_whales/test_logging.py
@@ -3,27 +3,8 @@ import logging
 import python_on_whales.utils
 
 
-def test_null_handler():
-    logger = logging.getLogger("python_on_whales")
-    assert any(isinstance(handler, logging.NullHandler) for handler in logger.handlers)
-
-
-def test_default_level_is_debug():
-    logger = logging.getLogger("python_on_whales")
-    assert logger.level == logging.DEBUG
-
-
-def test_logging_debug_env_var_disabled(caplog, monkeypatch):
-    monkeypatch.setenv("PYTHON_ON_WHALES_DEBUG", "0")
+def test_debug_level_logging(caplog):
     caplog.set_level(logging.DEBUG)
     command = ["echo", "hello"]
     python_on_whales.utils.run(command)
     assert " ".join(command) in caplog.text
-
-
-def test_logging_debug_env_var_enabled(caplog, monkeypatch):
-    monkeypatch.setenv("PYTHON_ON_WHALES_DEBUG", "1")
-    caplog.set_level(logging.DEBUG)
-    command = ["echo", "hello"]
-    python_on_whales.utils.run(command)
-    assert " ".join(command) not in caplog.text
